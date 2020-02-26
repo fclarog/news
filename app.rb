dad00d451ef80c7be76fa1ed1df67d7b36c915d6
fclarog <francisco.claro@kellogg.northwestern.edu>
2020-02-26 00:43:17 +0000

    3

--- app.rb
+++ app.rb
@@ -8,6 +8,8 @@ before { puts "Parameters: #{params}" }
 
 # enter your Dark Sky API key here
 ForecastIO.api_key = "c3e18d2e5103cb533deb1674c7ac4dc9"
+url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=07695ccbca25471d9d9972a36de857d2"
+
 
 get "/" do
   # show a view that asks for the location
@@ -16,7 +18,23 @@ get "/" do
 end
 
 get "/news" do
+    @news = HTTParty.get(url).parsed_response.to_hash
     results = Geocoder.search(params["location"])
+    @location = params["location"]
     lat_long = results.first.coordinates # => [lat, long]
-    "#{lat_long[0]} #{lat_long[1]}"
+    @latitude = lat_long[0]
+    @longitude = lat_long[1]
+    @lat_lng = "#{@latitude},#{@longitude}"
+    @forecast = ForecastIO.forecast(@latitude,@longitude).to_hash
+    @headlines = @news["articles"]
+    @current_temperature = @forecast["currently"]["temperature"]
+    @current_conditions = @forecast["currently"]["summary"]
+    @daily_forecast = @forecast["daily"]["data"]
+    @weekly_summary = @forecast["daily"]["summary"]
+   
+    view"news"
 end
+
+
+
+
